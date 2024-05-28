ee615687b833bb86794726b1ae29f84e6440e382
rcslbaa <ifasmarani3125@gmail.com>
2024-05-28 17:51:14 +0700

    menambahkan class seseorang (induk)

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -8,15 +8,3 @@ public:
 	//	cout << "Pesan dari seseorang" << endl;
 	//}
 };
-
-class joko : public seseorang {
-public:
-	void pesan() {
-		cout << "Pesan dari joko" << endl;
-	}
-};
-
-class lia : public seseorang {
-public:
-	
-};
